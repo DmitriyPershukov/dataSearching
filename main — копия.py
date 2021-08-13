be346b07398a7094a4eb916b9770a1aa4314ab08
DmitriyPershukov <55895822+DmitriyPershukov@users.noreply.github.com>
2021-08-13 17:16:39 +0500

    Add files via upload

--- main — копия.py
+++ main — копия.py
@@ -1,68 +1,54 @@
-
 import struct, os
 import json
 
-from decimal import Decimal 
-            
-def islisEmpty(list):
-    if len(list) != 0:
-        return False
-    else:
-        return True
+from decimal import Decimal
 
-def searchLongSequences(f, positionCounter, startingPositionCounter, count, number):
-    if number == "nan":
-        count[0] = 0
-        return
-    if number >= 0:
-        count[0] = count[0] +1
-        if count[0] == 0:                            
-            startingPositionCounter[0] = positionCounter
-    
-        if count[0] >= 32:
-            f.write(str(startingPositionCounter[0]) + ", ")
-    else:
-        count= 0
+typeArra= {"float":["f", 4], "int":["i", 4], "long long": ["q", 8], "double": ["d", 8]}
+searchingtype ="double"
+counterLength = 32
 
-def get(i):
-    return bytes([byte[i]])
 with open('390393_16-21-00.tdata','rb') as file:
-    f = open("float.txt", "a")
     result = []
-    byteStorer = b""
     byteStorer = file.read()
     counter = 0
     binarySize = 4
     bytetorerlen = os.stat('390393_16-21-00.tdata').st_size
-    for p in range(0, binarySize):
-        counter = 0
-        firs = 0
-        coun = 0
-        fourBytesStorer = b""
-        passing = False
-        for j in  range(p, bytetorerlen):
-            coun = coun + 1
-            fourBytesStorer +=  bytes([byteStorer[j]])
-            if coun == binarySize:
-                data = struct.unpack('f', fourBytesStorer)
-                coun = 0
-                fourBytesStorer = b""
-                if str(data)[1:-2] == "nan":
-                    number = -2
-                else:
-                    number = float(str(data)[1:-2])
-                gi = bytetorerlen
-                if number >= 0:
-                    counter = counter + 1
-                    if not passing:
-                        firs = j
-                    passing = True
-                elif number <0 or j ==gi:
-                    passing = False
-                    if counter >= 32:
-                        result.append({"pos": j, "len": counter})
-                    counter = 0
-    f.write(json.dumps(result))
-    f.close()
 
-    
+    n = 0
+    for o in typeArra:
+        result.append({o: []})
+        letter = typeArra[o][0]
+
+        for p in range(0, typeArra[o][1]):
+            counter = 0
+            firs = 0
+            coun = 0
+            fourBytesStorer = b""
+            passing = False
+            for j in  range(p, bytetorerlen):
+                coun = coun + 1
+                fourBytesStorer +=  bytes([byteStorer[j]])
+                if coun == typeArra[o][1]:
+                    data = struct.unpack(letter, fourBytesStorer)
+                    coun = 0
+                    fourBytesStorer = b""
+                    if str(data)[1:-2] == "nan":
+                        number = -2
+                    else:
+                        number = float(str(data)[1:-2])
+                    gi = bytetorerlen
+                    if number >= 0:
+                        counter = counter + 1
+                        if not passing:
+                            firs = j
+                        passing = True
+                    elif number <0 or j ==gi:
+                        passing = False
+                        if counter >= counterLength:
+                            result[n][o].append({"pos": j, "len": counter})
+                        counter = 0
+        n +=1
+    f = open("float.txt", "a")
+    for ip in result:   
+        f.write(json.dumps(ip))
+        f.write()
